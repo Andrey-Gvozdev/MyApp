2c6c4204bef4b11b609fc112f827dcd740e9fb0e
Andrey-Gvozdev <andrey.gvozdeu@gmail.com>
2021-12-01 16:26:16 +0300

    fixing column Content

--- MyApp/Models/Creative.cs
+++ MyApp/Models/Creative.cs
@@ -4,10 +4,13 @@ using System.ComponentModel.DataAnnotations;
 namespace MyProject.Models
 {
     [Index(nameof(Name), IsUnique = true)]
+    [Index(nameof(Content))]
     public class Creative
     {
         [Key]
         string Name { get; set; }
+
+        [Required]
         string Content { get; set; }
     }
 }
