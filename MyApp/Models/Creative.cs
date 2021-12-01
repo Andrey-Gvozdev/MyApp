dc7c341f7a438d1c7b36c8f86356bae0457ae6a4
Andrey Gvozdev <a.gvozdev@itransition.com>
2021-12-01 13:21:08 +0300

    creating Creative model

---
+++ MyApp/Models/Creative.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MyProject.Models
+{
+    [Index(nameof(Name), IsUnique = true)]
+    public class Creative
+    {
+        string Name { get; set; }
+        string Content { get; set; }
+    }
+}
