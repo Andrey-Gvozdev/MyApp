12fc0ee0d2e9962a4148df914706ab24e77a47af
Andrey-Gvozdev <andrey.gvozdeu@gmail.com>
2021-12-28 13:14:09 +0300

    adding ValidationNameIsFilled test

--- MyAppTests/ValidationServiceTests.cs
+++ MyAppTests/ValidationServiceTests.cs
@@ -1,24 +1,46 @@
 using Infrastructure;
-using Moq;
 using MyApp.Domain.Services;
 using NUnit.Framework;
-using System.ComponentModel.DataAnnotations;
 
 namespace MyAppTests;
 
 [TestFixture]
 public class ValidationServiceTests
 {
+    private static IValidationService MakeValidationService()
+    {
+        return new ValidationService();
+    }
     [TestCase("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", false)]
     [TestCase("validName", true)]
     public void ValidationNameLength_WhenCalled_ChangesWasNameValid(string name, bool expected)
     {
         bool result = true;
-        IValidationService validationService = new ValidationService();
+        IValidationService validationService = MakeValidationService();
 
         try
         {
             validationService.ValidationNameLength(name);
+        }
+        catch
+        {
+            result = false;
+        }
+
+        Assert.AreEqual(result, expected);
+    }
+
+    [TestCase("", false)]
+    [TestCase(" ", false)]
+    [TestCase("validName", true)]
+    public void ValidationNameIsFilled_WhenCalled_ChangesWasNameValid(string name, bool expected)
+    {
+        bool result = true;
+        IValidationService validationService = MakeValidationService();
+
+        try
+        {
+            validationService.ValidationNameIsFilled(name);
         }
         catch
         {
