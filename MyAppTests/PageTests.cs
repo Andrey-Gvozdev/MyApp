8585cacb24e3df8af80b893e091fa13b854be265
Andrey-Gvozdev <andrey.gvozdeu@gmail.com>
2021-12-29 15:19:15 +0300

    fixing test

--- MyAppTests/PageTests.cs
+++ MyAppTests/PageTests.cs
@@ -6,14 +6,19 @@ namespace MyAppTests;
 [TestFixture]
 public class PageTests
 {
+    private Page MakePage(string content)
+    {
+        return new Page("name", content);
+    }
+
     [TestCase("content", "<!DOCTYPE html>\n<html>\n<head></head>\n<body>\ncontent\n</body>\n</html>")]
     [TestCase("<body>\ncontent\n</body>", "<!DOCTYPE html>\n<html>\n<head></head>\n<body>\ncontent\n</body>\n</html>")]
     [TestCase("\n<head></head>\n<body>\ncontent\n</body>", "<!DOCTYPE html>\n<html>\n<head></head>\n<body>\ncontent\n</body>\n</html>")]
     [TestCase("<body>\ncontent\n</body>\n<head></head>\n", "<!DOCTYPE html>\n<html>\n<head></head>\n<body>\ncontent\n</body>\n</html>")]
     public void HtmlCorrector_CombinationsOfHtmlTags_ReturnsTrue(string content, string expectedContent)
     {
-        content = Page.HtmlCorrector(content);
+        var page = MakePage(content);
 
-        content.Should().Be(expectedContent);
+        page.Content.Should().Be(expectedContent);
     }
 }
