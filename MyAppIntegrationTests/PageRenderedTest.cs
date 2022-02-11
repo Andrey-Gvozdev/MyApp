3b3ac00c8ef698349b1a5074e6e819cf81dd3865
Andrey-Gvozdev <andrey.gvozdeu@gmail.com>
2022-02-11 15:55:33 +0300

    correcting test

--- MyAppIntegrationTests/PageRenderedTest.cs
+++ MyAppIntegrationTests/PageRenderedTest.cs
@@ -33,16 +33,21 @@ public class PageRenderedTest : IntegrationTest
     public async Task GetedPageIsExpectedPageTrue()
     {
         var expectedPage = new Page("expectedName", "content");
-        var postRequest = new HttpRequestMessage(HttpMethod.Post, "/api/page/");
         var testPageJson = JsonConvert.SerializeObject(expectedPage);
-        postRequest.Content = new StringContent(testPageJson, Encoding.UTF8, "application/json");
-        var postResponce = JsonConvert.DeserializeObject<Page>((await client.SendAsync(postRequest)).Content.ReadAsStringAsync().Result);
-        var getRequest = new HttpRequestMessage(HttpMethod.Get, $"/api/page/{postResponce.Id}");
-        getRequest.Content = new StringContent(postResponce.Id.ToString(), Encoding.UTF8, "application/json");
-        
-        var getResponce = await client.SendAsync(getRequest);
-        var result = JsonConvert.DeserializeObject<Page>((getResponce).Content.ReadAsStringAsync().Result);
+        var postRequest = new HttpRequestMessage(HttpMethod.Post, "/api/page/")
+        {
+            Content = new StringContent(testPageJson, Encoding.UTF8, "application/json")
+        };
+        var postResponce = await client.SendAsync(postRequest);
+        var postResult = JsonConvert.DeserializeObject<Page>(await postResponce.Content.ReadAsStringAsync());
+        var getRequest = new HttpRequestMessage(HttpMethod.Get, $"/api/page/{postResult.Id}")
+        {
+            Content = new StringContent(postResult.Id.ToString(), Encoding.UTF8, "application/json")
+        };
 
-        result.Name.Should().Be(expectedPage.Name);
+        var getResponce = await client.SendAsync(getRequest);
+        var getResult = JsonConvert.DeserializeObject<Page>(await getResponce.Content.ReadAsStringAsync());
+
+        getResult.Name.Should().Be(expectedPage.Name);
     }
 }
