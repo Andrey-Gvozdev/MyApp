0a9dda79e69169588666cc403bf07c851393fbee
Andrey-Gvozdev <andrey.gvozdeu@gmail.com>
2022-02-11 14:01:32 +0300

    creating GetedPageIsExpectedPageTrue test

--- MyAppIntegrationTests/PageRenderedTest.cs
+++ MyAppIntegrationTests/PageRenderedTest.cs
@@ -1,6 +1,7 @@
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using FluentAssertions;
 using Moq;
 using MyApp.Contracts.Events;
 using MyApp.Domain.DomainModel;
@@ -27,5 +28,22 @@ public class PageRenderedTest : IntegrationTest
         await client.SendAsync(postRequest);
 
         mockBus.Verify(x => x.Publish(It.Is<PageRendered>(e => e.Content == expectedContent), null), Times.Once);
+    }
+
+    [Fact]
+    public async Task GetedPageIsExpectedPageTrue()
+    {
+        var expectedPage = new Page("expectedName", "content");
+        var postRequest = new HttpRequestMessage(HttpMethod.Post, "/api/page/");
+        var testPageJson = JsonConvert.SerializeObject(expectedPage);
+        postRequest.Content = new StringContent(testPageJson, Encoding.UTF8, "application/json");
+        var postResponce = JsonConvert.DeserializeObject<Page>((await client.SendAsync(postRequest)).Content.ReadAsStringAsync().Result);
+        var getRequest = new HttpRequestMessage(HttpMethod.Get, $"/api/page/{postResponce.Id}");
+        getRequest.Content = new StringContent(postResponce.Id.ToString(), Encoding.UTF8, "application/json");
+        
+        var getResponce = await client.SendAsync(getRequest);
+        var result = JsonConvert.DeserializeObject<Page>((getResponce).Content.ReadAsStringAsync().Result);
+
+        result.Name.Should().Be(expectedPage.Name);
     }
 }
