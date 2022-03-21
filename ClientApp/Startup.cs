77d97461d9c4dd8c20a0170133a0a456f6a75b25
Andrey-Gvozdev <andrey.gvozdeu@gmail.com>
2022-03-21 18:07:26 +0300

    creating client app

---
+++ ClientApp/Startup.cs
@@ -0,0 +1,57 @@
+﻿using JavaScriptEngineSwitcher.ChakraCore;
+using JavaScriptEngineSwitcher.Extensions.MsDependencyInjection;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using React.AspNet;
+
+namespace ClientApp;
+public class Startup
+{
+    public Startup(IConfiguration configuration)
+    {
+        this.Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddMemoryCache();
+
+        services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+        services.AddReact();
+        services.AddJsEngineSwitcher(options => options.DefaultEngineName = ChakraCoreJsEngine.EngineName).AddChakraCore();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseBrowserLink();
+        }
+
+        app.UseReact(config =>
+        {
+            config
+              .AddScript("~/js/main.5ca9131f.js")
+              .SetJsonSerializerSettings(new JsonSerializerSettings
+              {
+                  StringEscapeHandling = StringEscapeHandling.EscapeHtml,
+                  ContractResolver = new CamelCasePropertyNamesContractResolver(),
+              });
+        });
+
+        app.UseHttpsRedirection();
+
+        app.UseDefaultFiles();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+/*        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });*/
+    }
+}
